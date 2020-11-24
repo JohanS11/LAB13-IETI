ed274ba3550f34427fd5d5ba90c1ab0e7d0ad006
Johan Arias <johan.arias@mail.escuelaing.edu.co>
2020-11-24 12:16:19 -0500

    part 1

---
+++ app/src/main/java/co/edu/eci/ieti/android/network/data/Task.java
@@ -0,0 +1,96 @@
+package co.edu.eci.ieti.android.network.data;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+import androidx.room.TypeConverters;
+
+import java.util.Date;
+
+import co.edu.eci.ieti.android.network.service.ToDate;
+
+@Entity(tableName = "task")
+public class Task {
+
+    @PrimaryKey
+    @NonNull
+    private long id;
+
+    private String description;
+
+    private int priority;
+
+    private User assignedTo;
+    @TypeConverters(ToDate.class)
+    private Date dueDate;
+
+    public Task()
+    {
+    }
+
+    @Ignore
+    public Task( long id, String description, int priority, Date dueDate )
+    {
+        this.id = id;
+        this.description = description;
+        this.priority = priority;
+        this.dueDate = dueDate;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription( String description )
+    {
+        this.description = description;
+    }
+
+    public int getPriority()
+    {
+        return priority;
+    }
+
+    public void setPriority( int priority )
+    {
+        this.priority = priority;
+    }
+
+    public Date getDueDate()
+    {
+        return dueDate;
+    }
+
+    public void setDueDate( Date dueDate )
+    {
+        this.dueDate = dueDate;
+    }
+
+    public void assignTo( User asignedTo )
+    {
+        this.assignedTo = asignedTo;
+    }
+
+    public User getAssignedTo()
+    {
+        return assignedTo;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId( long id )
+    {
+        this.id = id;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Task{" + "description='" + description + '\'' + ", priority=" + priority + ", dueDate=" + dueDate + '}';
+    }
+}
---
+++ app/src/main/java/co/edu/eci/ieti/android/network/service/ToDate.java
@@ -0,0 +1,20 @@
+package co.edu.eci.ieti.android.network.service;
+
+import androidx.room.TypeConverter;
+import androidx.room.TypeConverters;
+
+import java.util.Date;
+
+public class ToDate {
+
+    @TypeConverter
+    public static Date toDate(Long timestamp){
+        return timestamp == null ? null : new Date(timestamp);
+    }
+
+    @TypeConverter
+    public static Long toTimestamp(Date date) {
+        return date == null ? null : date.getTime();
+    }
+}
+
